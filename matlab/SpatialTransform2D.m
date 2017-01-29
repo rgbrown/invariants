84e84545d5c26e6bf832f535258fc720791d64ee
Richard Brown <rgbrown@gmail.com>
2017-01-30 10:54:25 +1300

    Add image transformations to SpatialTransform2D class

--- matlab/SpatialTransform2D.m
+++ matlab/SpatialTransform2D.m
@@ -3,4 +3,34 @@ classdef (Abstract) SpatialTransform2D
         forward(obj, x, y)
         reverse(obj, x, y)
     end
+    methods
+        function Fp = forward_image(obj, F, x, y)
+            nx = size(F, 2);
+            ny = size(F, 1);
+            x = check_xlim(x, nx);
+            y = check_xlim(y, ny);
+            [X, Y] = meshgrid(x, y);
+            [X_irreg, Y_irreg] = obj.reverse(X, Y);
+            Fp = interp2(X, Y, F, X_irreg, Y_irreg, 'cubic');
+        end
+        function Fp = reverse_image(obj, F, x, y)
+            nx = size(F, 2);
+            ny = size(F, 1);
+            x = check_xlim(x, nx);
+            y = check_xlim(y, ny);
+            [X, Y] = meshgrid(x, y);
+            [X_irreg, Y_irreg] = obj.forward(X, Y);
+            Fp = interp2(X, Y, F, X_irreg, Y_irreg, 'cubic');
+        end
+        
+    end
+end
+
+function x = check_xlim(x, nx)
+if numel(x) == 2
+    x = linspace(x(1), x(2), nx);
+elseif numel(x) ~= nx
+    error('x must be of the form [xmin, xmax], or a vector nx long');
+end
+
 end
