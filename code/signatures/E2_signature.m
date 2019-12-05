2a6a6162fdf9a2bdae206ac21ff01725b228ca3c
Richard Brown <rgbrown@gmail.com>
2019-12-06 09:29:19 +1300

    Tweaks to signatures

--- code/signatures/E2_signature.m
+++ code/signatures/E2_signature.m
@@ -3,10 +3,15 @@ derivative_order = 2;
 group = 'E2';
 out = signature_switch(f, @evaluate, derivative_order, group, varargin{:});
     function sig = evaluate(derivs)
-        [f, fx, fy, fxx, ~, fyy] = derivs{:}; % fxy not needed
+        [f, fx, fy, fxx, fxy, fyy] = derivs{:}; % fxy not needed
         I0 = f;
         I1 = fx.*fx + fy.*fy;
         I2 = fxx + fyy;
-        sig = {I0, I1, I2};
+        I3 = fx.^2.*fxx +2*fx.*fy.*fxy + fy.^2.*fyy;
+        I4 = fy.^2.*fxx -2*fx.*fy.*fxy + fx.^2.*fyy;
+        I5 = -fx.*fy.*(fxx - fyy) + (fx.^2 - fy.^2).*fxy;
+        I6 = fxx.^2 + fyy.^2 + 2*fxy.^2;
+        sig = {I0, I1, I5};
+       
     end
 end
--- code/signatures/SA2_signature.m
+++ code/signatures/SA2_signature.m
@@ -7,8 +7,12 @@ out = signature_switch(f, @evaluate, derivative_order, group, varargin{:});
         C = fxx.*fyy - fxy.^2;
         D = fy.^2.*fxx - 2*fx.*fy.*fxy + fx.^2.*fyy;
         E = fxxx.*fy.^3 - 3*fxxy.*fx.*fy.^2 + 3*fxyy.*fx.^2.*fy - fyyy.*fx.^3;
+        F = 2*fxy.^2-fxx.^2-fyy.^2;
+        I3 = fxxx.*fxyy.*fyy - fxxx.*fxy.*fyyy - fxxy.^2.*fyy + ...
+            fxxy.*fxx.*fyyy + fxxy.*fxyy.*fxy - fxx.*fxyy.^2;
         %F = fyy.*fxxy.^2 + fxx.*fxyy.^2 + fxyy.*fxxx.*fyyy - fyy.*fxxx.*fxyy - ...
         %fxyy.*fxxy.*fxyy - fxx.*fxxy.*fyyy;
+        %sig = {C, D, E};
         sig = {C, D, E};
     end
 end
