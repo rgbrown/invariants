dfdba43e481d4ce0f1f889befe7d690aa92bfaf5
Richard Brown <rgbrown@gmail.com>
2020-11-02 13:08:09 +1300

    playing with A2 signature

--- code/signatures/A2_new.m
+++ code/signatures/A2_new.m
@@ -4,14 +4,10 @@ group = 'A2';
 out = signature_switch(f, @evaluate, derivative_order, group, varargin{:});
     function sig = evaluate(derivs)
         [f, fx, fy, fxx, fxy, fyy, fxxx, fxxy, fxyy, fyyy] = derivs{:};
+        C = fx.^2.*fyy + fxx.*fy.^2 - 2*fx.*fxy.*fy;
+        D = fxx.*fyy - fxy.^2;
+        E = fx.^3.*fyyy - fxxx.*fy.^3 + 3*fx.*fxxy.*fy.^2 - 3*fx.^2.*fxyy.*fy;
         
-        I4_1 = fxx.*fyy-fxy.^2;
-        I4_2 = fx.^2.*fyy - 2*fx.*fy.*fxy + fy.^2.*fxx;
-        I6_1 = (fxx.*fxyy - 2*fxy.*fxxy + fyy.*fxxx).*fy - ...
-            (fxx.*fyyy - 2*fxy.*fxyy + fyy.*fxxy).*fx;
-        I6_2 = -fx.^3.*fyyy + 3*fx.^2.*fy.*fxyy - 3*fx.*fy.^2.*fxxy + ...
-            fy.^3.*fxxx;
-        
-        sig = {f, I4_2./(I4_1 + I4_2), I6_2./(I6_1 + I6_2)};
+        sig = {f, I1, I2};
     end
 end
