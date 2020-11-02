c9103827b94bd65a2065fa6e5feec40bfa8b6c90
Richard Brown <rgbrown@gmail.com>
2020-11-02 13:21:44 +1300

    Revert to the sphere projection signature for A(2)

--- code/signatures/A2_signature.m
+++ code/signatures/A2_signature.m
@@ -1,14 +1,13 @@
-function out = A2_signature(f, varargin)
+function out = A2_new(f, varargin)
 derivative_order = 3;
 group = 'A2';
 out = signature_switch(f, @evaluate, derivative_order, group, varargin{:});
     function sig = evaluate(derivs)
         [f, fx, fy, fxx, fxy, fyy, fxxx, fxxy, fxyy, fyyy] = derivs{:};
-        C = fxx.*fyy - fxy.^2;
-        D = fy.^2.*fxx - 2*fx.*fy.*fxy + fx.^2.*fyy;
-        E = fxxx.*fy.^3 - 3*fxxy.*fx.*fy.^2 + 3*fxyy.*fx.^2.*fy - fyyy.*fx.^3;
+        C = fx.^2.*fyy + fxx.*fy.^2 - 2*fx.*fxy.*fy;
+        D = fxx.*fyy - fxy.^2;
+        E = fx.^3.*fyyy - fxxx.*fy.^3 + 3*fx.*fxxy.*fy.^2 - 3*fx.^2.*fxyy.*fy;
         
-        denom = sqrt(C.^6 + D.^6 + E.^4);
-        sig = {f.*C.^3./denom, f.*D.^3./denom, f.*E.^2./denom};
+        sig = {f, I1, I2};
     end
 end
